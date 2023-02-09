a9a00ce1c90289fa03c99de2e60f8ada70472ce8
Meer Uzair <meer.uxair007@gmail.com>
2023-02-09 11:10:41 +0530

    Updated onclick events.
    cleaned some code

--- script.js
+++ script.js
@@ -3,10 +3,9 @@ let boxElOne = document.getElementById("box-el-one");
 let boxElTwo = document.getElementById("box-el-two");
 
 btn.addEventListener("click", () => {
-  let length = 8
+  let length = 8;
   let passwordCharacters =
-
-    "8BD1VAuGQbFfW6gZ9sNL7M0tTIX52Cepwy_4irdPESl.moJxkHnjRUhvcOa3qYKz";
+    "BDVAuGQbFfWgZsNLMtTIXCepwy_irdPESl.moJxkHnjRUhvcOaqYKz";
   let password1 = "";
 
   for (i = 0; i < length; i++) {
@@ -15,47 +14,35 @@ btn.addEventListener("click", () => {
   }
   boxElOne.textContent = password1;
 
+  let passwordDigits = "0123456789";
   let password2 = "";
 
   for (i = 0; i < length; i++) {
-    let randomNumber = Math.floor(Math.random() * passwordCharacters.length);
-    password2 += passwordCharacters[randomNumber];
+    let randomNumber = Math.floor(Math.random() * passwordDigits.length);
+    password2 += passwordDigits[randomNumber];
   }
 
   boxElTwo.textContent = password2;
 });
 
-// function generatePassword  (length = 8) {
-//   let passwordCharacters =
-//     "8BD1VAuGQbFfW6gZ9sNL7M0tTIX52Cepwy_4irdPESl.moJxkHnjRUhvcOa3qYKz";
-//   let password1 = "";
-
-//   for (i = 0; i < length; i++) {
-//     let randomNumber = Math.floor(Math.random() * passwordCharacters.length);
-//     password1 += passwordCharacters[randomNumber];
-//   }
-
-//   boxElOne.innerText = password1;
-
-//   let password2 = "";
-
-//   for (i = 0; i < length; i++) {
-//     let randomNumber = Math.floor(Math.random() * passwordCharacters.length);
-//     password2 += passwordCharacters[randomNumber];
-//   }
-
-//   boxElTwo.innerText = password2;
-
-// };
-
 let copyToClipboard = () => {
-  navigator.clipboard.writeText(boxElOne.innerText);
-  boxElOne.innerText = "Copied!";
-  boxElOne.style.display = "block";
+  if (boxElOne.innerText !== "PASSWORD") {
+    navigator.clipboard.writeText(boxElOne.innerText);
+    boxElOne.innerText = "Copied!";
+    boxElOne.style.display = "block";
+    setTimeout(() => {
+      boxElOne.innerText = "PASSWORD";
+    }, 2000);
+  }
 };
 
 let copyToClipboard2 = () => {
-  navigator.clipboard.writeText(boxElTwo.innerText);
-  boxElTwo.innerText = "Copied!";
-  boxElTwo.style.display = "block";
+  if (boxElTwo.innerText !== "PIN") {
+    navigator.clipboard.writeText(boxElTwo.innerText);
+    boxElTwo.innerText = "Copied!";
+    boxElTwo.style.display = "block";
+    setTimeout(() => {
+      boxElTwo.innerText = "PIN";
+    }, 2000);
+  }
 };
